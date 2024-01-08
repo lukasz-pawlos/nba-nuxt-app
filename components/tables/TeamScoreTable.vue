0e3e18582a407ff9ac93606fd77a680d499b0a85
lukasz-pawlos <252925@student.pwr.edu.pl>
2024-01-08 23:46:37 +0100

    Infinite Scroll

--- components/tables/TeamScoreTable.vue
+++ components/tables/TeamScoreTable.vue
@@ -10,7 +10,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="game in gamesData.data" :key="game.id">
+        <tr v-for="game in pageData" :key="game.id">
           <th>{{ game.date.split("T")[0] }}</th>
             <td>
               <nuxt-link :to="'/team/' + game.home_team.id">
@@ -62,14 +62,41 @@ const season = computed(() => localStorage.getItem("selectedYear"))
 
 const { data: gamesData } = await useFetch<FetchingData<Game[]>>(`https://www.balldontlie.io/api/v1/games`, { query: {
   ["team_ids[]"]: props.teamId,
-  ["seasons[]"]: season.value
+  ["seasons[]"]: season.value,
 } });
+
+const pageData = ref();
+const total_pages = ref();
+const current_page = ref();
 
 const pageTeamWin = (homeTeamId: number, homeScore: number, visitorScore: number): boolean => {
   const homeTeamWin = homeScore > visitorScore;
   return homeTeamId !== props.teamId ? homeTeamWin : homeTeamWin ? false : true
 }
 
+const getMoreData = () => {
+  window.onscroll = async () => {
+    let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
+    if (bottomOfWindow && total_pages.value !== current_page.value) {
+      const { data: gamesDatas } = await useFetch<FetchingData<Game[]>>(`https://www.balldontlie.io/api/v1/games`, { query: {
+      ["team_ids[]"]: props.teamId,
+      ["seasons[]"]: season.value,
+      page: current_page.value + 1
+      } });
+
+      const newData = pageData.value;
+      pageData.value = newData.concat(gamesDatas.value?.data);
+      total_pages.value = gamesDatas.value?.meta?.total_pages;
+      current_page.value = gamesDatas.value?.meta?.current_page;
+    };
+  }
+}
+onBeforeMount(() => {
+  pageData.value = gamesData.value?.data;
+  total_pages.value = gamesData.value?.meta?.total_pages;
+  current_page.value = gamesData.value?.meta?.current_page;
+})
+onMounted(() => getMoreData());
 </script>
 
 <style>
